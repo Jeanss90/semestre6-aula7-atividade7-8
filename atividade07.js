5e9ad766d61b7a6d03177b7104567f3caeb1e48a
Jean Santana <santanajean@hotmail.com>
2024-09-15 20:55:57 +0100

    programa criado com funcao fatorar

--- atividade07.js
+++ atividade07.js
@@ -1,5 +1,21 @@
 //Atividade 07
 //programa para calculo de fatorial
 
-var num  = Math.floor(Math.random() * 101);
+//gerando numero para fatorial
+var num  = Math.floor(Math.random() * 16);
 console.log(num);
+
+//criando funcao para fatorar
+function fatorial(x) {
+    if (x < 0) 
+        return -1;
+    else if (x == 0) 
+        return 1;
+    else {
+        return (x * fatorial(x - 1));
+    }
+}
+
+
+console.log(fatorial(num));
+
